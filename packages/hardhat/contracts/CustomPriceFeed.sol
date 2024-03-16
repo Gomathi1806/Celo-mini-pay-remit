da05bc6af85a94d8a057451d4f33a101372048fb
Gomathi T <62291940+Gomathi1806@users.noreply.github.com>
2024-03-16 17:03:42 +0000

    Update CustomPriceFeed.sol
    Custom Price feed Contract for price.

--- packages/hardhat/contracts/CustomPriceFeed.sol
+++ packages/hardhat/contracts/CustomPriceFeed.sol
@@ -1,1 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+contract CustomPriceFeed {
+    mapping(bytes32 => uint256) public exchangeRates;
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function setExchangeRate(bytes32 currencyKey, uint256 rate) external {
+        require(msg.sender == owner, "Only owner can set exchange rates");
+        exchangeRates[currencyKey] = rate;
+    }
+
+    function getExchangeRate(bytes32 currencyKey) public view returns (uint256) {
+        return exchangeRates[currencyKey];
+    }
+}
 
